53fa8febe0f87ad192bae5367bcb30cda75bc078
harputsamed <harputsamed@gmail.com>
2025-03-05 22:54:14 +0300

    project flask 01-02 added

--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -9,5 +9,5 @@ def number():
      y=36
      return render_template('body.html', value1=x, value2=y, sum=x*y)
 if __name__== "__main__":
-     # app.run(debug=True, port=30000)
-     app.run(host= '0.0.0.0', port=8080)
+     app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8080)
